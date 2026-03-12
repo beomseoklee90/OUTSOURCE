7633f142fa37d006af8df0fd16c048c27b07a484
ボムソク\beoms <beomseoklee90@gmail.com>
2026-03-12 23:24:44 +0900

    please

--- next.config.mjs
+++ next.config.mjs
@@ -4,10 +4,6 @@ const nextConfig = {
   // 1. 레포지토리 이름이 'outsource'이므로 정확히 입력
   basePath: "/outsource",
 
-  // 2. assetPrefix를 basePath와 동일하게 하되 끝에 슬래시를 빼거나,
-  // 아예 이 줄을 삭제해 보세요. (basePath가 이미 이 역할을 수행함)
-  assetPrefix: "https://beomseoklee90.github.io/outsource",
-
   images: {
     unoptimized: true,
   },
