ba87bf824f5be4befe2b74081663ad8b63cc43ad
ボムソク\beoms <beomseoklee90@gmail.com>
2026-03-12 22:57:48 +0900

    next.config.mjs basePath fixed

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
 const nextConfig = {
-  output: "export", // 필수: 정적 파일로 뽑아내기
-  images: { unoptimized: true }, // 필수
+  output: "export",
+  // 레포지토리 이름을 '/' 뒤에 넣어주세요. (예: '/my-portfolio')
+  // 만약 유저 메인 페이지(아이디.github.io)라면 필요 없습니다.
+  basePath: "/OUTSOURCE",
+  images: { unoptimized: true },
 };
-export default nextConfig;
